0093ec9379563075b3703ec2237d2ca1ef7fd04b
Kitsune7X <fox.dev7@proton.me>
2026-04-16 10:00:25 -0700

    initial commit

---
+++ chapter01/HelloCS/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#");
+
+// Revealing the namespace of the Program class
+string name = typeof(Program).Namespace ?? "<null>";
+
+Console.WriteLine($"Namespace: {name}");
+
+throw new Exception();
