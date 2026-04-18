85045b25582f9c293753bdeb07b06295894eac9a
Kitsune7X <fox.dev7@proton.me>
2026-04-18 09:33:44 -0700

    object

---
+++ chapter02/Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿object height = 1.88; // Storing a double in an object
+object name = "Fox"; // Storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall.");
+
+// int length1 = name.Length; // Should give compiler error!
+int length2 = ((string)name).Length; // Cast name to a string.
+Console.WriteLine($"{name} has {length2} characters.");
