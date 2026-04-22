36c58ef1f3f0f11a3c4541dd22c26ec83740836f
Kitsune7X <fox.dev7@proton.me>
2026-04-22 12:18:24 -0700

    .

---
+++ chapter02/Formatting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization; // To use CultureInfo.
+// Set current culture to US English
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+Console.WriteLine(format: "{0} apples cost {1:C}", arg0: numberOfApples, arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(format: "{0} apples cost {1:C}", arg0: numberOfApples, arg1: pricePerApple * numberOfApples);
+
+// Three parameter values can use named arguments.
+Console.WriteLine("{0} {1} lived in {2}.", arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+
+// Four or more parameter values cannot use named arguments.
+Console.WriteLine("{0} {1} lived in {2} and worked in the {3} team at {4}.",
+"Roger", "Cevung", "Stockholm", "Education", "Optimizely");
+
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+// Format String
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+Console.WriteLine();
+Console.WriteLine(format: "{0,-10} {1,6}", arg0: "Name", arg1: "Count");
+Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: applesText, arg1: applesCount);
+Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: bananasText, arg1: bananasCount);
+
+
+double value = 123;
+Console.WriteLine(value.ToString("00000"));
+Console.WriteLine(String.Format("{0:00000}", value));
--- chapter02/Variables/Program.cs
+++ chapter02/Variables/Program.cs
@@ -1,75 +1,93 @@
 ﻿using System.Drawing;
-using System.Dynamic; // For ExpandoObject.
+using System.Dynamic;
 using System.Xml;
 
-object height = 1.88; // Storing a double in an object
-object name = "Fox"; // Storing a string in an object
-Console.WriteLine($"{name} is {height} metres tall.");
+// For ExpandoObject.
 
-// int length1 = name.Length; // Should give compiler error!
-var length2 = ((string)name).Length; // Cast name to a string.
-Console.WriteLine($"{name} has {length2} characters.");
+// object height = 1.88; // Storing a double in an object
+// object name = "Fox"; // Storing a string in an object
+// Console.WriteLine($"{name} is {height} metres tall.");
 
-// Dynamic type
-dynamic something;
-// Storing an array of int values in a dynamic object.
-// An array of any type has a Length property.
-something = new[] { 3, 5, 7 };
-// Storing an int in a dynamic object.
-// int does not have a Length property.
-something = 12;
-// Storing a string in a dynamic object.
-// string has a Length property.
-something = "Foxy";
-// This compiles but might throw an exception at run-time.
-Console.WriteLine($"The length of something is {something.Length}");
-// Output the type of something variable.
-Console.WriteLine($"something is a {something.GetType()}");
+// // int length1 = name.Length; // Should give compiler error!
+// var length2 = ((string)name).Length; // Cast name to a string.
+// Console.WriteLine($"{name} has {length2} characters.");
 
-#region ExpandoObject
+// // Dynamic type
+// dynamic something;
+// // Storing an array of int values in a dynamic object.
+// // An array of any type has a Length property.
+// something = new[] { 3, 5, 7 };
+// // Storing an int in a dynamic object.
+// // int does not have a Length property.
+// something = 12;
+// // Storing a string in a dynamic object.
+// // string has a Length property.
+// something = "Foxy";
+// // This compiles but might throw an exception at run-time.
+// Console.WriteLine($"The length of something is {something.Length}");
+// // Output the type of something variable.
+// Console.WriteLine($"something is a {something.GetType()}");
 
-dynamic person = new ExpandoObject();
-// Add properties.
-person.FirstName = "John";
-person.LastName = "Doe";
-person.Age = 30;
-Console.WriteLine($"{person.FirstName} {person.LastName} is  {person.Age} years old");
-// Cast the ExpandoObject into a dictionary.
-var dictionary = (IDictionary<string, object>)person;
-// Each item in the dictionary is a key-value pair.
-foreach (var item in dictionary) Console.WriteLine($"{item.Key} = {item.Value}");
+// #region ExpandoObject
 
-#endregion
+// dynamic person = new ExpandoObject();
+// // Add properties.
+// person.FirstName = "John";
+// person.LastName = "Doe";
+// person.Age = 30;
+// Console.WriteLine($"{person.FirstName} {person.LastName} is  {person.Age} years old");
+// // Cast the ExpandoObject into a dictionary.
+// var dictionary = (IDictionary<string, object>)person;
+// // Each item in the dictionary is a key-value pair.
+// foreach (var item in dictionary) Console.WriteLine($"{item.Key} = {item.Value}");
 
-// Specifying the type of a local variable
-var population = 67_000_000; // UK population.
-var weight = 1.88; // in kg.
-var price = 4.99M; // in pounds sterling.
-var fruit = "Apples";
-var letter = 'Z';
-var happy = true;
+// #endregion
+
+// // Specifying the type of a local variable
+// var population = 67_000_000; // UK population.
+// var weight = 1.88; // in kg.
+// var price = 4.99M; // in pounds sterling.
+// var fruit = "Apples";
+// var letter = 'Z';
+// var happy = true;
 
 
-// Compare good and bad use cases of var
-// Good use of var, avoid repeated type as shown
-// in the more verbose 2nd statement.
-var xml1 = new XmlDocument();
-XmlDocument xml2 = new XmlDocument();
+// // Compare good and bad use cases of var
+// // Good use of var, avoid repeated type as shown
+// // in the more verbose 2nd statement.
+// var xml1 = new XmlDocument();
+// var xml2 = new XmlDocument();
 
-// Bad use of var because we cannot tell the type,
-// we should use a specific type declaration as shown in
-// the 2nd statement.
-var file1 = File.CreateText("something1.txt");
-StreamWriter file2 = File.CreateText("something2.txt");
+// // Bad use of var because we cannot tell the type,
+// // we should use a specific type declaration as shown in
+// // the 2nd statement.
+// var file1 = File.CreateText("something1.txt");
+// var file2 = File.CreateText("something2.txt");
 
-/* Value types have memory allocated on the stack automatically */
-short age; // Allocates 2 bytes of memory on the stack to store a System.Int16 value.
-long population; // Allocates 8 bytes of memory on the stack to store a System.Int64 value.
-DateTime birthdate; // Allocates 8 bytes of memory on the stack to store a System.DateTime value.
-Point location; // Allocates 8 types of memory on the stack to store a System.Drawing.Point value.
+// /* Value types have memory allocated on the stack automatically */
+// short age; // Allocates 2 bytes of memory on the stack to store a System.Int16 value.
+// long population; // Allocates 8 bytes of memory on the stack to store a System.Int64 value.
+// DateTime birthdate; // Allocates 8 bytes of memory on the stack to store a System.DateTime value.
+// Point location; // Allocates 8 types of memory on the stack to store a System.Drawing.Point value.
 
-/* Reference types will only have memory allocated on the heap when new is used (but they automatically have 
-some memory allocated on the stack to store information about themselves including the memory address of where they are
-on the heap). */
+// /* Reference types will only have memory allocated on the heap when new is used (but they automatically have
+// some memory allocated on the stack to store information about themselves including the memory address of where they are
+// on the heap). */
 
-// Person bob; // Allocate memory in the stack that can point to a Person object in the heap. Initially, bob will have the value null.
+// // Person bob; // Allocate memory in the stack that can point to a Person object in the heap. Initially, bob will have the value null.
+
+// // When using `new` keyword.
+// age = 45; // Initialize this variable to 45 using a literal value.
+// population = 68_000_000; // Initialize this variable to 68 million using literal value.
+// birthdate = new(1995, 2, 23); // Initialize this variable to Feb 23, 1995.
+// location = new(10, 20); // Initialize the X and Y coordinates of this value.
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string) ?? "<NULL>"}");
+
+int number = 13;
+Console.WriteLine($"number set to: {number}");
+number = default;
+Console.WriteLine($"number reset to its default: {number}");
